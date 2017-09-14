8dd6f375b37c605a937e5b9f938825f2dd2101e4
Frances Coronel <frances.coronel@accenture.com>
2017-09-14 11:10:37 -0700

    Service workers causing trouble
    Signed-off-by: Frances Coronel <frances.coronel@accenture.com>

--- src/assets/js/service-worker.js
+++ src/assets/js/service-worker.js
@@ -1,35 +1,35 @@
 let version = '1.0';
 
-self.addEventListener("install", e => {
+self.addEventListener('install', e => {
   let timeStamp = Date.now();
   e.waitUntil(
-    caches.open("pacific-shores-shuttle").then(cache => {
-      return cache
-        .addAll([
+    caches.open('pacific-shores-shuttle').then(cache => {
+      return cache.addAll([
           `/`,
-          `assets/js/min/main.min.js?timestamp=${timeStamp}`,
-          `assets/css/main.css?timestamp=${timeStamp}`,
           `assets/favicons/favicon.ico?timestamp=${timeStamp}`,
-          `assets/img/teaser.png?timestamp=${timeStamp}`,
-          `assets/img/logo-circle.svg?timestamp=${timeStamp}`
+          `assets/img/stops/1200.png?timestamp=${timeStamp}`,
+          `assets/img/stops/1400.png?timestamp=${timeStamp}`,
+          `assets/img/stops/1700.png?timestamp=${timeStamp}`,
+          `assets/img/stops/1900.png?timestamp=${timeStamp}`,
+          `assets/img/stops/caltrain.png?timestamp=${timeStamp}`,
+          `assets/img/map.png?timestamp=${timeStamp}`,
+          `assets/img/logo.svg?timestamp=${timeStamp}`
         ])
         .then(() => self.skipWaiting());
     })
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches
-      .match(event.request, {
-        ignoreSearch: true
-      })
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request, {
+      ignoreSearch: true
+    }).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
